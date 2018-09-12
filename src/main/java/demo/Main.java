67c363e2421a81aa8ef0311511be6fe99235d75e
zohdi muharam <zohdimuharam@zohdis-MacBook.local>
2018-09-12 13:19:58 -0400

    impleanted business logic

---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,544 @@
+//package demo;
+//
+//import java.util.Scanner;
+//
+//public class Main {}
+//    public static void main(String[] args) {
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+
+
+
+    //----------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    //public static void main(String[] args) {
+//       /* System.out.println("hello world");
+//        System.out.println("zohdi");
+//
+//
+//    int myInt = 200;
+//
+//    short myshorthort = 3000;
+//
+//    long myLong = 7000l;
+//
+//    byte myByte = 100;
+//
+//    float myFloat = 2.3f;
+//
+//    double myDouble = 1.89;
+//
+//    char myChar = 'Y';
+//
+//    boolean myBool = true;
+//
+//
+//    String favArtist = "My fave artist is \t bigger";
+//
+//
+//    System.out.println("myint");
+//    System.out.println("myshort");
+//    System.out.println("mylong");
+//    System.out.println("mybyte"); */
+//
+//
+//    //  }
+////}
+//
+//
+///*public class main {
+//    public static void main(String[] args)
+//        String all = String.join("zohdi", "mike", "man", "lol", "zman"); // all
+//}
+//*/
+//
+////StringBuilder builder = new StringBuilder();
+////builder.append("z")
+//
+////    public static void main(String[] args) {
+////
+////        Scanner input = new Scanner(System.in);
+////
+////        System.out.println("what is this number");
+////
+////        long digits = input.nextLong();
+////
+////        System.out.println("Your number is" + digits);
+////    }
+////}
+//
+//
+//    /*public static void main(String[] args) {
+//
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("what is your name");
+//
+//        String getname = input.nextLine();
+//
+//        System.out.println("my name is " + getname);
+//
+//        System.out.println("what is your number");
+//
+//        long digit = input.nextLong();
+//
+//        System.out.println("your number is" + digit);
+//
+//
+//        System.out.println("are you DTF");
+//        boolean dtf = input.nextBoolean();
+//        System.out.println("cool");
+//
+//
+//    }
+//}
+//
+//*/
+///*
+//public static void main(String[] args) {
+//int num1, num2, sum;
+//Scanner sc = new Scanner(System.in);
+//System.out.println("Enter First Number: ");
+//num1 = sc.nextInt();
+//
+//System.out.println("Enter Second Number: ");
+//num2 = sc.nextInt();
+//
+//sc.close();
+//sum = num1 + num2;
+//System.out.println("Sum of these numbers: " + sum);
+//int num3, num4, product;
+//Scanner sc = new Scanner(System.in);
+//System.out.println("enter first number: ");
+//num3 = sc.nextInt();
+//
+//System.out.println("enter second number:");
+//num4 = sc.nextInt();
+//
+//sc.close();
+//product = num3 * num4;
+//System.out.println("product of these numbers: " * product);
+//
+//
+//int num5, num6, reminder;
+//Scanner sc = new Scanner(System.in);
+//System.out.println("enter first number: ");
+//num5 = sc.nextInt();
+//
+//System.out.println("enter second number:");
+//num6 = sc.nextInt();
+//
+//sc.close();
+//reminder = num5 % num6;
+//System.out.println("product of these numbers: " * reminder);
+//}
+//}
+//public static void main(String[] args) {
+//// i should not type above here
+//
+//Scanner input = new Scanner(System.in);
+//System.out.println("give me a number:");
+//int num1 = input.nextInt();
+//
+//System.out.println("give me another number");
+//int num2 = input.nextInt();
+//
+//int answer = num1 + num2;
+//
+//int a = num1 * num2;
+//
+//
+//System.out.println("the sum of " + num1 + "and" + num2 + "is" + answer );
+//
+//System.out.println("the product of " + num1 + "and" + num2 + "is" + a);
+//}
+//// I should not type above here
+//
+//
+//{
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/extra/S/main.java
@@ -0,0 +1,76 @@
+package extra.S;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+       //do not color above here
+
+//       int i = 1;
+//       System.out.println("welcome to my site and enter your password");
+//
+//       do {
+//
+//           System.out.println("trial # " + i);
+//           System.out.println("incorrect password - try again");
+//
+//           int = 1++;
+//
+//       } while (1 > 4);
+//        {
+//
+//
+//            int i = 1;
+//            System.out.println("please enter you name");
+//
+//        {
+//
+//            System.out.println("");
+//
+//
+//        }
+
+
+            Scanner input = new Scanner(System.in);
+
+                System.out.println("username");
+                String getname = input.nextLine(); // allows you type on the next line
+
+
+
+                String name1 = "bob"; // these are varible
+                String name2 = "Alice";
+                while(name1){          // this is for a loop
+                    System.out.println("wrong name please provide me with your name");
+                    if(name1.equalsIgnoreCase("bob")) {   // it's a true or false
+
+                        System.out.println("welcome bob");
+                    }
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       //do not color below here
+
+
+
+
+    }
+}
